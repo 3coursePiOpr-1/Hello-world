5892c70646a81e51a333255bf11476789a892027
ArkharovDanil <maslinaraz@gmail.com>
2023-10-03 12:43:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConsoleApp4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
+}
